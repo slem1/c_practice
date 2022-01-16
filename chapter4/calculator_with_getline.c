1561eb9afbcb66ef772692000fd2e403e04bdbb3
Sylvain Lemoine <sylvain.lem1@gmail.com>
2022-01-16 15:37:32 +0100

    wi^

--- chapter4/calculator_with_getline.c
+++ chapter4/calculator_with_getline.c
@@ -11,11 +11,12 @@ double atof_scientific(char s[]);
 void ungetchs(char s[]);
 void ungetch_all();
 int getLine(char s[]);
+void calculator();
 
 void playstack();
 
 int getch();
-int getopt(char[]);
+int getopt(char s[], char input[]);
 
 int buf[BUFSIZE];
 int bufp = 0;
@@ -46,84 +47,35 @@ void printTopStack(){
     }
 }
 
-void duplicateTopStack(){
-    if(sp > 0){
-        push(val[sp-1]);
-    }
-}
-
-void swaptop(){
-    if(sp > 1){
-        double top = pop();
-        double previous = pop();
-        push(top);
-        push(previous);
-    }
-}
-
-void clear(stack){
-    sp = 0;
-}
-
-void printStack(){
-    for(int i=0; i < sp; i++){
-        printf("stack[%d]=%g\n", sp, val[i]);
-    }
-}
-
+int position = 0;
 
 int main(){
-    playstack();
+    
+    char input_buf[100];
 
-    ungetch_all();
+    char input[100] = "10 15 *";
 
-    char line[10000];
+    calculator();
 
-    while(getLine(line)){
 
-       getopt(line)
-    }
     
 }
 
-void playstack(){
-
-    push(4);
-
-    push(5);
-
-  //  printStack();
-
-    printTopStack();
-
-    swaptop();
-
-   // printStack();
-
-    duplicateTopStack();
-
-    printStack();
-
-}
-
-
-void ungetch_all(){
-
-    ungetchs("laplace");
-
-    for(int i =0; i<8; i++){
-        int c = getch();
-        printf("%c\n", c);
-    }
-}
-
-
 void calculator(){
+
+    char line[100];
     char input_buf[100];
     int type;
     double op2;
 
-    while((type = getopt(input_buf)) != EOF){
+    while(getLine(line)){    
+
+        position = 0;
+        type =0;
+
+        while(type != '\n'){
+            type = getopt(input_buf,line);
+
         switch(type){
             case NUMBER: 
                 push(atof_scientific(input_buf));
@@ -162,63 +114,40 @@ void calculator(){
                 printf("unknow command: %s\n", input_buf);
                 break;
         }
+
+        }
+        
+    
     }
 }
 
-int getch(){
-    if(bufp > 0){
-        return buf[--bufp];
-    }else{
-        return getchar();
-    }
-}
+int getopt(char s[], char input[]){
 
-void ungetch(int c){
-    if(bufp >= BUFSIZE){
-        printf("ungetch: too many characters");
-    }
-    buf[bufp++] = c;
-}
-
-void ungetchs(char s[]){
-
-    for(int i=0; s[i] != '\0'; i++){
-        ungetch(s[i]);
-    }
-}
-
-int getopt(char s[]){
-
-    // printf("buf[%d] = %d \n", bufp, buf[bufp - 1]);
     int c;
-    while((c = s[0] = getch()) == ' ' || c == '\t');
+    while((c = s[0] = input[position++]) == ' ' || c == '\t');
 
     s[1] = '\0';
+
+    if(input[position] == '\0'){
+        position--;
+        return '\n';
+    }
 
     if(!isdigit(c)){
         return c;
     }
 
+    position--;
+
     int i = 0;
 
     if(isdigit(s[0])){    
-        while(isdigit(s[++i] = c = getch()));
+        while(isdigit(s[++i] = c = input[++position]));
     }
 
-    if(s[i] == '.'){
-        while(isdigit(s[++i] = c = getch()));        
-    }
-
-    if(c != EOF){
-      //  printf("unget of %d\n", c);
-        ungetch(c);
-    }
-
-    s[++i] = '\0';
+    s[i] = '\0';
 
     return NUMBER;
 
 }
-
-
 
